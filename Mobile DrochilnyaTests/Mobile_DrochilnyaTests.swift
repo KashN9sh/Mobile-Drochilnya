670593ed37bccb6014b77390130bd3ec30da5ce0
KorolevRoman228 <kash244@yandex.ru>
2025-09-13 22:08:19 +0300

    Initial Commit

---
+++ Mobile DrochilnyaTests/Mobile_DrochilnyaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_DrochilnyaTests.swift
+//  Mobile DrochilnyaTests
+//
+//  Created by Roman on 13.09.2025.
+//
+
+import Testing
+@testable import Mobile_Drochilnya
+
+struct Mobile_DrochilnyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
